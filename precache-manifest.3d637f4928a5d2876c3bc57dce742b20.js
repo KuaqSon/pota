a984b2c85b749ddea0a5dd77ec9f23fbf3d60b4d
Son Nguyen <qson.ng@gmail.com>
2020-08-20 16:35:54 +0700

    Updates

---
+++ precache-manifest.3d637f4928a5d2876c3bc57dce742b20.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39443daa2058fd6164363367d28ee6af",
+    "url": "/pota/index.html"
+  },
+  {
+    "revision": "87d8e3d954c98d9b77dc",
+    "url": "/pota/static/css/main.756b00ac.chunk.css"
+  },
+  {
+    "revision": "71dc35b14cb334f83593",
+    "url": "/pota/static/js/2.f72029bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pota/static/js/2.f72029bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87d8e3d954c98d9b77dc",
+    "url": "/pota/static/js/main.ec43f731.chunk.js"
+  },
+  {
+    "revision": "02439ac94839e99cd9ab",
+    "url": "/pota/static/js/runtime-main.b14c66e5.js"
+  },
+  {
+    "revision": "2de2027e26b3f12e9cb311ff41fcfa3d",
+    "url": "/pota/static/media/all.2de2027e.svg"
+  },
+  {
+    "revision": "6538478dc19c18af7f53ab40cd3cace8",
+    "url": "/pota/static/media/command.6538478d.svg"
+  },
+  {
+    "revision": "5c02f5d8662d02a7ccefaca395545a57",
+    "url": "/pota/static/media/doing.5c02f5d8.svg"
+  },
+  {
+    "revision": "37455a2ef6538e14a5fb871bab122f72",
+    "url": "/pota/static/media/done.37455a2e.svg"
+  },
+  {
+    "revision": "5b9fdc1b050e0d60b1fde799ebbfca57",
+    "url": "/pota/static/media/schedule.5b9fdc1b.svg"
+  }
+]);
